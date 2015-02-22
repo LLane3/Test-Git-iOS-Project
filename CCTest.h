76364859f4768834ea19862356cddabb4ce3818f
David Eddy <deddy@DEAi20.local>
2015-02-22 15:58:03 -0500

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by David Eddy on 2015-02-22.
+//  Copyright (c) 2015 GamingTheWord. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
